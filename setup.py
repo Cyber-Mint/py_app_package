170577ea31ff154b2bcd969107ae1b2611bdaa93
Bank-Builder <andrew.turpin@cyber-mint.com>
2020-06-04 18:33:45 +0200

    Upload to PyPi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as file:
     
     
 setup(name='py_app_package',
-    version='0.1.3',
+    version='0.1.4',
     description='A packaging template application ',
     long_description=long_description,
     long_description_content_type='text/x-rst',
