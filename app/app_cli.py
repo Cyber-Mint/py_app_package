74096601ae063b7b9e4c1718621f0f56da16f5cb
Bank-Builder <andrew.turpin@cyber-mint.com>
2020-06-04 12:08:56 +0200

    Initial commit

--- app/app_cli.py
+++ app/app_cli.py
@@ -1,13 +1,5 @@
-#from io import StringIO
-#from contextlib import redirect_stdout
 import app
 
 def main():
    print(app.say_hello())
-
-#f = StringIO()
-#with redirect_stdout(f):
-#    print('foobar')
-#    print(12)
-#print('Got stdout: "{0}"'.format(f.getvalue()))
-
+   
